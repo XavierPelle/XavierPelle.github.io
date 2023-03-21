f81fb8ea27f6e2eec69b6878a27c3f2b62c50dbe
TheVroum <36685559+TheVroum@users.noreply.github.com>
2023-03-21 14:42:58 +0100

    fixes and footer occupy script

--- JS/footer_anim.js
+++ JS/footer_anim.js
@@ -1,29 +1,33 @@
 const footer = document.querySelector("footer");
 let scrollAgain = 1;
-footer.onscroll = () => console.log("scrolled");
-const cpr = document.querySelector("footer>p");
-setInterval(()=>console.log(cpr.getBoundingClientRect().top - window.innerHeight > -100), 500);
-setInterval(()=>console.log(cpr.getBoundingClientRect().top - window.innerHeight), 500);
+const tEl = document.querySelector("footer>section>h3");
 
+let ci = null;
+
+const a = ()=> {
+    console.log(tEl.getBoundingClientRect().top - window.innerHeight);
+    if(tEl.getBoundingClientRect().top - window.innerHeight < 0)
+    {
+        footer.classList.add("occupy-screen");
+        if (ci !== null)
+            clearInterval(ci);
+        ci = setInterval(c, 100);
+        // setTimeout(window.scrollTo(0, document.body.scrollHeight), 200);
+        setTimeout(window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" }), 200);
+    }
+};
+
+
+const c = () => {
+    if(tEl.getBoundingClientRect().top - window.innerHeight > 100)
+    {
+        if (ci !== null)
+            clearInterval(ci);
+        ci = setInterval(a, 100);
+        footer.classList.remove("occupy-screen");
+    }
+};
 
 setTimeout(()=>{
-    setInterval(()=> {
-        if(cpr.getBoundingClientRect().top - window.innerHeight < -40)
-            footer.classList.add("occupy-screen")
-            if(scrollAgain)
-            {
-                scrollAgain = false;
-                // setTimeout(()=>window.scrollTo({top: document.body.scrollHeight, behavior: "smooth"}), 10);
-            }
-    }, 300);
-    
-    
-    setInterval(()=> {
-        if(cpr.getBoundingClientRect().top - window.innerHeight > 340)
-        {
-            footer.classList.remove("occupy-screen")
-            // scrollAgain = true;
-        }
-    }, 300);
-
+    ci = setInterval(a, 100);
 }, 800);
