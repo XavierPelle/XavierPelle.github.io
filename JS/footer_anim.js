00fb5b00fc3b10b613a0386708fceaa2daa38d41
TheVroum <36685559+TheVroum@users.noreply.github.com>
2023-03-21 15:12:32 +0100

    adjusted footer anim thesholds

--- JS/footer_anim.js
+++ JS/footer_anim.js
@@ -6,20 +6,19 @@ let ci = null;
 
 const a = ()=> {
     console.log(tEl.getBoundingClientRect().top - window.innerHeight);
-    if(tEl.getBoundingClientRect().top - window.innerHeight < 0)
+    if(tEl.getBoundingClientRect().top - window.innerHeight < 150)
     {
         footer.classList.add("occupy-screen");
         if (ci !== null)
             clearInterval(ci);
         ci = setInterval(c, 100);
-        // setTimeout(window.scrollTo(0, document.body.scrollHeight), 200);
         setTimeout(window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" }), 200);
     }
 };
 
 
 const c = () => {
-    if(tEl.getBoundingClientRect().top - window.innerHeight > 100)
+    if(tEl.getBoundingClientRect().top - window.innerHeight > 250)
     {
         if (ci !== null)
             clearInterval(ci);
